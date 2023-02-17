cdf10d8d3270ab2d348747947acc6b301b8cd09e
Akanchha <akanchhasharma2310@gmail.com>
2023-02-17 15:02:19 +0530

    Main.js width commit

--- src/components/main.js
+++ src/components/main.js
@@ -6,9 +6,9 @@ const Main = () => {
         <>
           <Container>
             <div className="d-flex justify-content-center my-4">
-              <span style={{backgroundColor: "#6c757d", borderRadius: "2em", width: "min-content"}} className="d-flex p-1">
-                <Button style={{ borderRadius: "2em"}} className="px-5">Explore</Button>
-                <Button variant="secondary" style={{ borderRadius: "2em"}} className="px-5">Discover</Button>
+              <span style={{backgroundColor: "#6c757d", borderRadius: "2em", width: "100%"}} className="d-flex p-1">
+                <Button style={{ borderRadius: "2em",width:"50%"}} className="px-5">Explore</Button>
+                <Button variant="secondary" style={{ borderRadius: "2em",width:"50%"}} className="px-5">Discover</Button>
               </span>
             </div>
             <PostLayout />
